d3dc4797de8b016671abec6dedfb0047255567aa
kyubinjin <2376282@ewhain.net>
2024-04-05 21:52:38 +0900

    style: stylelint 제거

---
+++ src/pages/Makerspage/Makerspage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Makerspage = () => {
+  return (
+    <>
+      <Test>테스트</Test>
+    </>
+  );
+};
+
+export default Makerspage;
+
+const Test = styled.div`
+  display: flex;
+  z-index: 1;
+  width: 100%;
+  background-color: red;
+`;
