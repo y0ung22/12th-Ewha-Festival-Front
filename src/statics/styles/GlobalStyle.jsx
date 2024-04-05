50d5adc139687fa93ee865953762552d4a6ee42a
kyubinjin <2376282@ewhain.net>
2024-04-05 16:58:21 +0900

    style: globalstyle 설정

---
+++ src/statics/styles/GlobalStyle.jsx
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+${reset}
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+input, textarea {
+    border: none;
+    outline: none;
+}
+
+html, body {
+    height: 100%;
+    background: linear-gradient(180deg, #FFF 80%, #00FF75 100%);
+}
+
+@media (min-width: 576px) {
+    .wrapper {
+        width: 390px;
+        margin: 0 auto;
+    }
+}
+`;
+
+export default GlobalStyles;
