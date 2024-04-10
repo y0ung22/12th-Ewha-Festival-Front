7e45b4b0e2f2e9f5615c9c0163011911a7f72cc8
youtheyeon <yuseoyeon@ewhain.net>
2024-04-11 00:02:01 +0900

    feat : TopBar & Footer 구현

---
+++ src/_common/Footer.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+//images
+import likelion from '../assets/icons/footer-likelion.png';
+import instagram from '../assets/icons/footer-instagram.svg';
+import github from '../assets/icons/footer-github.svg';
+
+const Footer = () => {
+  return (
+    <Wrapper>
+      <div>멋쟁이 사자처럼 12기 | Likelion Ewha - 12th</div>
+      <div>
+        <a href='https://linktr.ee/likelion.ewha'>
+          <img src={likelion} alt='likelion logo' />
+        </a>
+        <a
+          href='
+        https://www.instagram.com/likelion_ewha/ '
+        >
+          <img src={instagram} alt='instagram logo' />
+        </a>
+        <a href='https://github.com/EWHA-LIKELION'>
+          <img src={github} alt='github logo' />
+        </a>
+      </div>
+      <div>Copyright ⓒ Likelion Ewha 12th. All Rights Reserved.</div>
+    </Wrapper>
+  );
+};
+
+export default Footer;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 158.08px;
+  flex-shrink: 0;
+  background: #efefef;
+  border-top: 1.04px solid #9b9b9b;
+
+  color: #9b9b9b;
+  text-align: center;
+
+  div:nth-child(1) {
+    margin-top: 20.84px;
+    height: 34px;
+    font-size: 10.4px;
+    font-weight: 500;
+    line-height: 16.64px;
+  }
+
+  div:nth-child(2) {
+    margin-top: 19.16px;
+    margin-bottom: 19.37px;
+    display: flex;
+    gap: 16px;
+  }
+
+  div:nth-child(3) {
+    font-size: 8.32px;
+    font-weight: 300;
+    line-height: normal;
+  }
+`;
