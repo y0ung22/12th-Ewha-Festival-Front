7c2418ee581d29abec8881112dd184d058f2bb49
youtheyeon <yuseoyeon@ewhain.net>
2024-04-13 20:32:46 +0900

    style : 공통 버튼 스타일 추가

---
+++ src/_common/Button.jsx
@@ -0,0 +1,37 @@
+import { styled } from 'styled-components';
+
+export const GrayBtn = styled.div`
+  display: inline-flex;
+  padding: 7px 17px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 30px;
+  border: 1px solid var(--gray02);
+  background: var(--gray03);
+
+  color: var(--gray01);
+  text-align: center;
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 20px;
+  letter-spacing: -0.5px;
+`;
+
+export const GreenBtn = styled.div`
+  display: inline-flex;
+  padding: 7px 17px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 30px;
+  border: 1px solid var(--green02);
+  background: var(--green_01);
+
+  color: var(--wh);
+  text-align: center;
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 20px;
+  letter-spacing: -0.5px;
+`;
