2b61b052edafe3ba41f379e4a3b37f55fb5ee39e
youtheyeon <yuseoyeon@ewhain.net>
2024-04-25 03:50:35 +0900

    feat : 공지사항 관련 페이지 모달 추가

---
+++ src/pages/NoticePage/components/TitleInput.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { S } from '../Notice.style';
+
+const TitleInput = ({ handleTitle }) => {
+  return (
+    <>
+      <S.GreenLine />
+      <S.TitleInput
+        type='text'
+        placeholder='제목을 작성하세요(최대 20자)'
+        maxLength={20}
+        onChange={handleTitle}
+      />
+      <S.GreenLine />
+    </>
+  );
+};
+
+export default React.memo(TitleInput);
