71b1b2fbbe3a0b484c46e6e8e9fd7688460225d9
mod_siw <pvq_71@ewhain.net>
2024-04-29 11:14:29 +0900

    [chore] app.js 수정

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
+import SignupKakaoPage from './pages/SignupPage/SignupKakaoPage';
 import BoothEditPage from './pages/AdminEditPage/BoothEditPage';
+
 import MenuEditPage from './pages/AdminEditPage/MenuEditPage';
 import MenuEditDetailPage from './pages/AdminEditPage/MenuEditDetailPage';
 import MenuAddPage from './pages/AdminEditPage/MenuAddPage';
 import MakersPage from './pages/Makerspage/Makerspage';
 
-import MainPage from './pages/MainPage/MainPage';
-import BoothListPage from './pages/ListPage/BoothListPage';
-import SearchPage from './pages/SearchPage/SearchPage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import DetailPage from './pages/DetailPage/DetailPage';
+
+import NoticeListPage from './pages/NoticePage/NoticeListPage';
+import NoticeDetailPage from './pages/NoticePage/NoticeDetailPage';
+import NoticeWritePage from './pages/NoticePage/NoticeWritePage';
+import NoticeEditPage from './pages/NoticePage/NoticeEditPage';
+
+import MyPage from './pages/MyPage/MyPage';
+import FacilityPage from './pages/FacilityPage/FacilityPage';
+import ProgramListPage from './pages/ProgramPage/ProgramListPage';
+import ProgramDetailPage from './pages/ProgramPage/ProgramDetailPage';
 
 function App() {
   return (
@@ -22,7 +33,21 @@ function App() {
         <Routes>
           <Route path={'/login'} element={<LoginPage />}></Route>
           <Route path={'/signup'} element={<SignupPage />}></Route>
-          <Route path={'/boothedit'} element={<BoothEditPage />}></Route>
+          <Route path={'/signupkakao'} element={<SignupKakaoPage />}></Route>
+          <Route path={'/detail/:id'} element={<DetailPage />} />
+          <Route path={'/about'} element={<AboutPage />} />
+
+          <Route path={'/boothedit'} element={<BoothEditPage />} />
+          <Route path={'/menuedit'} element={<MenuEditPage />} />
+          <Route path={'/menuedit/:id'} element={<MenuEditDetailPage />} />
+          <Route path={'/menuadd'} element={<MenuAddPage />} />
+
+          <Route path={'/notice'} element={<NoticeListPage />} />
+          <Route path={'/notice/:id'} element={<NoticeDetailPage />} />
+          <Route path={'/notice/write'} element={<NoticeWritePage />} />
+          <Route path={'/notice/edit'} element={<NoticeEditPage />} />
+
+          <Route path={'/makers'} element={<MakersPage />} />
         </Routes>
       </BrowserRouter>
     </>
