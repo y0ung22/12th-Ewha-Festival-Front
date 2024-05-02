4e2e4f1c28f5946e8de8c56ef0daa5a32c3dd5d0
youtheyeon <yuseoyeon@ewhain.net>
2024-05-02 14:08:39 +0900

    fix : 토큰 저장 방법 수정

--- src/api/auth.js
+++ src/api/auth.js
@@ -9,8 +9,9 @@ export const PostSignup = async (user_id, password, nickname) => {
       nickname: nickname
     });
     console.log(response.data);
-    const { accessToken } = response.data.data.access_token;
-    http.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+    const expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + 72 * 60 * 60 * 1000);
+    document.cookie = `token=${response.data.data.access_token}; expires=${expirationDate.toUTCString()};`;
     alert('가입이 완료되었습니다.');
     return Promise.resolve(response.data);
   } catch (error) {
@@ -26,8 +27,9 @@ export const PostLogin = async (user_id, password) => {
       password: password
     });
     console.log(response.data);
-    const { accessToken } = response.data.data.access_token;
-    http.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+    const expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + 72 * 60 * 60 * 1000);
+    document.cookie = `token=${response.data.data.access_token}; expires=${expirationDate.toUTCString()};`;
     return Promise.resolve(response);
   } catch (error) {
     if (error.response && error.response.status === 400) {
--- src/api/http.js
+++ src/api/http.js
@@ -5,3 +5,21 @@ export const http = axios.create({
   contentType: 'application/json; charset=utf-8;',
   withCredentials: true
 });
+
+const token = getCookie('token') || false;
+
+http.defaults.headers.common['Authorization'] = token
+  ? `Bearer ${token}`
+  : null;
+
+function getCookie(name) {
+  const cookieString = document.cookie;
+  const cookies = cookieString.split('; ');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].split('=');
+    if (cookie[0] === name) {
+      return cookie[1];
+    }
+  }
+  return null;
+}
