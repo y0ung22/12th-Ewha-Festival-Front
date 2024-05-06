e18c4329d213ce1477d207c423c0f2f58389e627
JiwonChoi0805 <won0805@ewhain.net>
2024-05-06 20:15:14 +0900

    style : 로그인/회원가입 ui

--- src/pages/MyPage/components/UserInfo.jsx
+++ src/pages/MyPage/components/UserInfo.jsx
@@ -28,6 +28,7 @@ const UserInfo = () => {
 
   return (
     <>
+      <Title>마이페이지</Title>
       <User>
         <Name>{nickname} 님</Name>
         <LogoutBtn onClick={() => Logout()}>로그아웃</LogoutBtn>
@@ -37,6 +38,24 @@ const UserInfo = () => {
 };
 
 export default UserInfo;
+
+const Title = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  margin-bottom: 1rem;
+  color: var(--bk01);
+  text-align: center;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.625rem; /* 108.333% */
+  letter-spacing: -0.03125rem;
+  margin-top: 2.31rem;
+`;
 
 const User = styled.div`
   display: flex;
@@ -48,7 +67,7 @@ const User = styled.div`
   border-radius: 0.9375rem;
   border: 1px solid var(--gray03);
   background: linear-gradient(158deg, #43ff9966, #f7f7f7);
-  margin: 5.81rem 0 2.5rem 0;
+  margin: 1.87rem 0 2.5rem 0;
 `;
 
 const Name = styled.div`
