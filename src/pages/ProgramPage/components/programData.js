85036a8f3fb4bb22cb674fd44fed4321950420c2
kyubinjin <2376282@ewhain.net>
2024-05-07 02:29:18 +0900

    style: programlistpage 기본이미지 설정

--- src/pages/ProgramPage/components/programData.js
+++ src/pages/ProgramPage/components/programData.js
@@ -1,4 +1,4 @@
-import ex from '../images/programEx.svg';
+import defaultImg from '../images/program-default.png';
 
 export const programData = {
   수: [
@@ -8,7 +8,7 @@ export const programData = {
       time: '11:00 - 14:00',
       summary:
         '이화여자대학교 대동제만의 개막식. 학생, 교수, 학내 노동자 등 이화의 구성원들이 함께 비빔밥을 비비고 나누어 먹으며 모두가 함께 만들어나가는 대동제를 알립니다.',
-      thumbnail: ex
+      thumbnail: defaultImg
     }
   ],
   목: [
@@ -18,7 +18,7 @@ export const programData = {
       time: '12:30 - 13:30',
       summary:
         '누가누가 초록과 제일 잘 어울릴까요? 바로바로 이화그린을 사랑하는 당신! 당신을 이화그린 앰버서더로 임명합니다. 대동제의 한가운데서 인간 이화그린이 되어보세요.',
-      thumbnail: ex
+      thumbnail: defaultImg
     },
     {
       name: '야시장',
@@ -26,7 +26,7 @@ export const programData = {
       time: '17:00 - 22:00',
       summary:
         '대강당 근처에서 푸드트럭과 플리마켓을 즐기실 수 있습니다. 선선한 여름 날 밤 소소한 먹거리들과 즐길거리들이 이화인을 찾아갑니다.',
-      thumbnail: ex
+      thumbnail: defaultImg
     },
     {
       name: '야간 영화제',
@@ -34,7 +34,7 @@ export const programData = {
       time: '19:00 - 21:00',
       summary:
         '시원한 여름밤, 가까운 사람들과 함께 잔디광장에서 영화를 감상할 수 있습니다. 야시장과 함께 즐겨보세요.',
-      thumbnail: ex
+      thumbnail: defaultImg
     }
   ],
   금: [
@@ -44,14 +44,14 @@ export const programData = {
       time: '13:30 - 15:00',
       summary:
         '올해로 40주년을 맞이한 영산 줄다리기. 모두가 함께 줄을 꼬고 줄다리기를 하며 대동의 의미를 느껴보는 시간입니다.',
-      thumbnail: ex
+      thumbnail: defaultImg
     },
     {
       name: '아티스트 공연',
       place: '잔디광장',
       time: '18:30 - 21:30',
       summary: '대동제의 마지막 날 밤, 특별한 아티스트들이 이화인들을',
-      thumbnail: ex
+      thumbnail: defaultImg
     }
   ]
 };
