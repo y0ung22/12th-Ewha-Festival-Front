46f2af53d85b24d3ecda080b2b2be0df88bb6685
mod_siw <jh04011368@gmail.com>
2024-05-07 04:13:29 +0900

    fix : 반응형 수정

--- src/pages/MainPage/components/MainMenuCard.jsx
+++ src/pages/MainPage/components/MainMenuCard.jsx
@@ -49,18 +49,14 @@ export default MainMenuCard;
 const Card = styled.div`
   position: relative;
 
-  @media (max-width: 410px) {
-    aspect-ratio: ${props =>
-      props.size === 'small' ? '9.125 / 11.375' : '10.625 / 12.3125'};
-    width: 100%;
-    height: 100%;
-  }
+  aspect-ratio: 9.125 / 11.375;
+  width: 100%;
+  height: 100%;
 
-  width: 9.125rem;
-  height: 11.375rem;
+  max-height: 11.375rem;
 
   flex-shrink: 0;
-  border-radius: ${props => (props.size === 'small' ? '0.9375rem' : '20px')};
+  border-radius: 0.9375rem;
   box-shadow: 0px 0px 9px 0px rgba(255, 255, 255, 0.25) inset;
   overflow: hidden;
 
