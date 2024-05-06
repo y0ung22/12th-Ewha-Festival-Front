4080951678f7c5628592432140da41de8a8a955c
변지혜 <127821462+mod-siw@users.noreply.github.com>
2024-05-07 02:38:04 +0900

    chore: 메인페이지 수정

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -31,7 +31,7 @@ export default MainPage;
 const Wrapper = styled.div`
   position: relative;
   width: 100%;
-  height: 120vh;
+  height: 105vh;
   overflow: hidden;
 
   -webkit-user-select: none;
