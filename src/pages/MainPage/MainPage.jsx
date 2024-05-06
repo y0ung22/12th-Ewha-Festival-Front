bdb09e560a64be13b486eb04cec264b66da2a05c
youtheyeon <yuseoyeon@ewhain.net>
2024-05-06 19:01:16 +0900

    feat : 메인페이지 바텀시트 애니메이션 구현

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -15,7 +15,6 @@ const MainPage = () => {
         <TopBar isWhite={true} isMenu={true} isMain={true} />
         <ScrapBook />
         <MainBottomSheet />
-        <Footer />
       </ContentWrapper>
     </Wrapper>
   );
@@ -26,7 +25,7 @@ export default MainPage;
 const Wrapper = styled.div`
   position: relative;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   overflow: hidden;
 `;
 
