b586505b9bb2ad8a809394d6f32cd8349ab7eec4
mod_siw <jh04011368@gmail.com>
2024-05-07 15:22:42 +0900

    fix: 반응형 수정

--- src/_common/ScrapCard.jsx
+++ src/_common/ScrapCard.jsx
@@ -45,8 +45,10 @@ export default ScrapCard;
 const Card = styled.div`
   position: relative;
 
-  @media (max-width: 380px) {
+  @media (max-width: 460px) {
+    aspect-ratio: 10.625 / 12.3125;
     width: 100%;
+    height: 100%;
   }
 
   width: ${props => (props.size === 'small' ? '9.125rem' : '10.625rem')};
