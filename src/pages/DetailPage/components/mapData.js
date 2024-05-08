be006245a68c884a2ee7b99ffd8e8b0380fae5e3
youtheyeon <yuseoyeon@ewhain.net>
2024-05-08 09:55:56 +0900

    feat : 부스 상세페이지 지도 추가

---
+++ src/pages/DetailPage/components/mapData.js
@@ -0,0 +1,57 @@
+import maingate from '../images/maingate.png';
+import auditorium from '../images/auditorium.png';
+import studentunion from '../images/studentunion.png';
+import humanecology from '../images/humanecology.png';
+import humanities from '../images/humanities.png';
+import posco from '../images/posco.png';
+import ecc from '../images/ecc.png';
+import education from '../images/education.png';
+import shinsegae from '../images/shinsegae.png';
+
+export const mapData = [
+  {
+    id: 1,
+    location: '정문',
+    img: maingate
+  },
+  {
+    id: 2,
+    location: '대강당',
+    img: auditorium
+  },
+  {
+    id: 3,
+    location: '학문관',
+    img: studentunion
+  },
+  {
+    id: 4,
+    location: '생활관',
+    img: humanecology
+  },
+  {
+    id: 5,
+    location: '학관',
+    img: humanities
+  },
+  {
+    id: 6,
+    location: '포스코관',
+    img: posco
+  },
+  {
+    id: 7,
+    location: '휴웃길',
+    img: ecc
+  },
+  {
+    id: 8,
+    location: '교육관',
+    img: education
+  },
+  {
+    id: 9,
+    location: '신세계관',
+    img: shinsegae
+  }
+];
